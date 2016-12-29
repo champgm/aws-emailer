7e1240752e5786f9463fbc6efa6fb2882e5e5711
Mac Champion <mac.champion@careerbuilder.com>
2016-12-29 15:31:22 -0500

    Starting to work on lambda + serverless

--- web-frontend/index.php
+++ web-frontend/index.php
@@ -9,8 +9,6 @@
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-
-  <script src="js/focus.js"></script>
 </head>
 
 <body>
@@ -33,6 +31,7 @@
         </div>
     </form>
   </div>
+  <script src="js/focus.js"></script>
 </body>
 
 </html>
